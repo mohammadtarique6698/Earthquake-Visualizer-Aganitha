261d8e231021738d55c434a34083576b3799ae80
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2024-11-10 22:06:16 +0530

    Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import axios from 'axios';
 import 'leaflet/dist/leaflet.css';
--- src/components/EarthquakeDetails.jsx
+++ src/components/EarthquakeDetails.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const EarthquakeDetails = ({ selectedEarthquake, closeDetails }) => (
   <div
     className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center"
